ac515d8436d39485d51fec53b6edcee5f7860098
rijulsrivastava <rijulsrivastva@gmail.com>
2026-08-26 22:07:50 +0530

    SideMenu is created with working toggle button

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from './components/Header'
+import SideMenu from './components/SideMenu';
 
 function App() {
+
+  const [isMenuOpen, setIsMenuOpen] = useState(true)
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  }
+
   return (
     <div className='m-5'>
-      <Header />
+      <Header toggleMenu={toggleMenu} />
+      {isMenuOpen ? <SideMenu /> : null}
     </div>
   )
 }
---
+++ Frontend/src/components/SideMenu.jsx
@@ -0,0 +1,58 @@
+import { TiHome } from "react-icons/ti"
+import { SiYoutubeshorts, SiYoutubegaming } from "react-icons/si"
+import { MdSubscriptions, MdHistory, MdWatchLater, MdOutlineNewspaper } from "react-icons/md"
+import { BiSolidLike } from "react-icons/bi"
+import { IoMdTrendingUp, IoIosMusicalNotes } from "react-icons/io"
+
+function SideMenu() {
+    return (
+        <aside className="flex flex-col gap-2 mt-5 w-[15%]">
+            <div className=" flex gap-2 ml-2 items-center">
+                <TiHome />
+                <span>Home</span>
+            </div>
+            <div className=" flex gap-2 ml-2 items-center">
+                <SiYoutubeshorts />
+                <span>Shorts</span>
+            </div>
+            <div className=" flex gap-2 ml-2 items-center">
+                <MdSubscriptions />
+                <span>Subscriptions</span>
+            </div>
+            <hr />
+            <h4 className="ml-2">You</h4>
+            <div className=" flex gap-2 ml-2 items-center">
+                <MdHistory />
+                <span>History</span>
+            </div>
+            <div className=" flex gap-2 ml-2 items-center">
+                <MdWatchLater />
+                <span>Watch Later</span>
+            </div>
+            <div className=" flex gap-2 ml-2 items-center">
+                <BiSolidLike />
+                <span>Liked Videos</span>
+            </div>
+            <hr />
+            <h4 className="ml-2">Explore</h4>
+            <div className=" flex gap-2 ml-2 items-center">
+                <IoMdTrendingUp />
+                <span>Trending</span>
+            </div>
+            <div className=" flex gap-2 ml-2 items-center">
+                <IoIosMusicalNotes />
+                <span>Music</span>
+            </div>
+            <div className=" flex gap-2 ml-2 items-center">
+                <SiYoutubegaming />
+                <span>Gaming</span>
+            </div>
+            <div className=" flex gap-2 ml-2 items-center">
+                <MdOutlineNewspaper />
+                <span>News</span>
+            </div>
+        </aside>
+    )
+}
+
+export default SideMenu
