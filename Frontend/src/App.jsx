41a65df022d7c6700a72aa320037a402fa35d1c9
rijulsrivastava <rijulsrivastva@gmail.com>
2026-08-27 23:33:59 +0530

    react-router has been incorporated in the app

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,6 +4,7 @@ import SideMenu from './components/SideMenu';
 import HomePage from './components/HomePage';
 import RegisterUser from './components/RegisterUser';
 import Login from './components/Login';
+import { Outlet } from 'react-router';
 
 function App() {
 
@@ -15,12 +16,12 @@ function App() {
   return (
     <div className='m-5'>
       <Header toggleMenu={toggleMenu} />
-      {/* <div className='flex gap-5 mt-5'>
+      <div className='flex gap-5 mt-5'>
         {isMenuOpen ? <SideMenu /> : null}
-        <HomePage />
-      </div> */}
-      {/* <RegisterUser /> */}
-      <Login />
+        <main className="flex-1">
+          <Outlet />
+        </main>
+      </div>
     </div>
   )
 }
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,9 +2,29 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router'
+import HomePage from './components/HomePage.jsx'
+import Login from './components/Login.jsx'
+import RegisterUser from './components/RegisterUser.jsx'
+
+const appProvider = createBrowserRouter([{
+  path: '/',
+  element: <App />,
+  children: [{
+    path: '/',
+    element: <HomePage />
+  },
+  {
+    path: '/login',
+    element: <Login />
+  },
+  {
+    path: '/register',
+    element: <RegisterUser />
+  }
+  ]
+}])
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <RouterProvider router={appProvider} />
 )
